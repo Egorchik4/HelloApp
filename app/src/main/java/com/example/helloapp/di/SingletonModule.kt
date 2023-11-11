6fe444a236f72b3d969214507d1e93d9ed7dfcd1
Egorchik4 <EgorDanyluk72@mail.ru>
2023-11-11 16:21:34 +0700

    remove the domain connection to the presentation

--- app/src/main/java/com/example/helloapp/di/SingletonModule.kt
+++ app/src/main/java/com/example/helloapp/di/SingletonModule.kt
@@ -1,6 +1,5 @@
 package com.example.helloapp.di
 
-import android.content.Context
 import com.example.helloapp.data.api.WeatherApi
 import com.example.helloapp.data.datasource.RetrofitBuilder
 import com.example.helloapp.data.datasource.network.WeatherDataSource
@@ -8,12 +7,10 @@ import com.example.helloapp.data.datasource.network.WeatherDataSourceImpl
 import com.example.helloapp.data.repository.WeatherRepositoryImpl
 import com.example.helloapp.domain.repository.WeatherRepository
 import com.example.helloapp.domain.usecase.GetWeatherUseCase
-import com.example.helloapp.domain.usecase.ShowSnackBarUseCase
 import com.example.helloapp.domain.usecase.ValidationUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -49,11 +46,5 @@ class SingletonModule {
 	@Singleton
 	fun provideValidationUseCase(): ValidationUseCase {
 		return ValidationUseCase()
-	}
-
-	@Provides
-	@Singleton
-	fun provideSnackBarUseCase(@ApplicationContext context: Context): ShowSnackBarUseCase {
-		return ShowSnackBarUseCase(context)
 	}
 }
